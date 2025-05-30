bd5c2f952aac209855fec3291bc5336a9e2616ff
DESKTOP-V4K9IKO\giki0 <g.iki.0329@gmail.com>
2025-05-31 02:25:46 +0900

    remove local code

--- api/rss.js
+++ api/rss.js
@@ -71,13 +71,4 @@ app.get('/rss', async (req, res) => {
   }
 });
 
-
-const PORT = process.env.PORT || 3000;
-if (require.main === module) {
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
-}
-
-
 module.exports = app;
